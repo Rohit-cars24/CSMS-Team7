0b028b3850d2fcab38e5a00b326c6a810ca8a174
Tejasvp24 <tejas.p@cars24.com>
2025-01-24 01:09:56 +0530

    Delete CSMS directory

--- CSMS/src/main/java/com/cars24/csms/exceptions/UsesServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.cars24.csms.exceptions;
-
-public class UserServiceException extends RuntimeException{
-    public UserServiceException(String message){
-        super(message);
-    }
-
-}
